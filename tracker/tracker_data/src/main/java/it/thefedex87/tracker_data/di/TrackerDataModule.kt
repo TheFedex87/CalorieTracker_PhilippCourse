7c8aaf4bf14699bace5468f913f65f51fbf09c90
Federico <federico.creti87@gmail.com>
2022-02-13 18:23:38 +0100

    Added remote api and local database

---
+++ tracker/tracker_data/src/main/java/it/thefedex87/tracker_data/di/TrackerDataModule.kt
@@ -0,0 +1,52 @@
+package it.thefedex87.tracker_data.di
+
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import it.thefedex87.tracker_data.local.TrackerDatabase
+import it.thefedex87.tracker_data.remote.OpenFoodApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TrackerDataModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient{
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOpenFoodApi(client: OkHttpClient): OpenFoodApi {
+        return Retrofit.Builder()
+            .baseUrl(OpenFoodApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
+            .build()
+            .create(OpenFoodApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providesTrackerDatabase(app: Application): TrackerDatabase {
+        return Room.databaseBuilder(
+            app,
+            TrackerDatabase::class.java,
+            "tracker_db"
+        ).build()
+    }
+}
---
+++ tracker/tracker_data/src/main/java/it/thefedex87/tracker_data/remote/dto/SearchDto.kt
@@ -0,0 +1,5 @@
+package it.thefedex87.tracker_data.remote.dto
+
+data class SearchDto(
+    val products: List<Product>,
+)
